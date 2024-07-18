25239d9b6d52c74b458cc456421f7fb3a027359a
ishaan19d <ishaandas848@gmail.com>
2024-07-18 18:58:32 +0530

    Corrected get_key error

--- Backend/subscription/api/urls.py
+++ Backend/subscription/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PurchaseSubscriptionView, PaymentCallbackView, get_key
+from .views import PurchaseSubscriptionView, PaymentCallbackView
 
 urlpatterns = [
     path('purchase/', PurchaseSubscriptionView.as_view(), name='purchase'),
