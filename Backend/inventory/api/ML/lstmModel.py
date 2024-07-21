167f846bfad2de6ee995c88e8b484990336dc97b
Ubaid Abbas <68930485+ubaid2751@users.noreply.github.com>
2024-07-21 20:50:27 +0530

    Update lstmModel.py

--- Backend/inventory/api/ML/lstmModel.py
+++ Backend/inventory/api/ML/lstmModel.py
@@ -6,112 +6,134 @@ from sklearn.preprocessing import RobustScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM, Bidirectional, BatchNormalization
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam, RMSprop
 from tensorflow.keras.regularizers import l2
 
 import warnings
 warnings.filterwarnings('ignore')
 from DataPreprocess.DataProcessing import DataPreprocessing
 
-# Load and preprocess the data
-df = pd.read_csv(r'DataPreprocess\data\Sales Data.csv')
-proc = DataPreprocessing()
-df_proc = proc.process_data(df)
+class SalesPredictionModel:
+    def __init__(self, data_path, lookback:int=30):
+        self.data_path = data_path
+        self.scaler = RobustScaler()
+        self.lookback = lookback
+        self.model = None
+        self.future_predictions = []
 
-X = df_proc.loc[:, ['order date', 'sales']]
-X['order date'] = pd.to_datetime(X['order date'])
-X['order date'] = X['order date'].dt.to_period('D')
-X = X.groupby('order date').sum()
+    def load_data(self):
+        df = pd.read_csv(self.data_path)
+        proc = DataPreprocessing()
+        df_proc = proc.process_data(df)
 
-data = X.values
-data = data.reshape((-1, 1))
+        X = df_proc.loc[:, ['order date', 'sales']]
+        X['order date'] = pd.to_datetime(X['order date'])
+        X['order date'] = X['order date'].dt.to_period('D')
+        X = X.groupby('order date').sum()
 
-# Scale the data
-scaler = RobustScaler()
-data = scaler.fit_transform(data)
+        data = X.values
+        data = data.reshape((-1, 1))
+        data = self.scaler.fit_transform(data)
 
-# Function to create dataset for LSTM
-def Create_Dataset(df, lookback=1):
-    X, Y = [], []
-    for i in range(len(df) - lookback - 1):
-        X.append(df[i:(i + lookback), 0])
-        Y.append(df[i + lookback, 0])
-    return np.array(X), np.array(Y)
+        return data
 
-# Prepare the dataset with lookback period
-lookback = 30
-X_data, Y_data = Create_Dataset(data, lookback)
+    def create_dataset(self, df, lookback=1):
+        X, Y = [], []
+        for i in range(len(df) - lookback - 1):
+            X.append(df[i:(i + lookback), 0])
+            Y.append(df[i + lookback, 0])
+        return np.array(X), np.array(Y)
 
-X_data = np.reshape(X_data, (X_data.shape[0], 1, X_data.shape[1]))
+    def build_model(self, input_shape):
+        model = Sequential()
+        model.add(Bidirectional(LSTM(500, activation='tanh', return_sequences=True, kernel_regularizer=l2(0.1)), input_shape=input_shape))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        model.add(Bidirectional(LSTM(250, activation='tanh', kernel_regularizer=l2(0.1))))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        model.add(Dense(128, activation='relu', kernel_regularizer=l2(0.1)))
+        model.add(BatchNormalization())
+        model.add(Dense(1, activation='linear'))
 
-# Build the model
-print("Model Training Starts.......")
-model = Sequential()
-model.add(Bidirectional(LSTM(1000, activation='tanh', return_sequences=True, kernel_regularizer=l2(0.1)), input_shape=(X_data.shape[1], X_data.shape[2])))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))
-model.add(Bidirectional(LSTM(500, activation='tanh', kernel_regularizer=l2(0.1))))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))
-model.add(Dense(128, activation='relu', kernel_regularizer=l2(0.1)))
-model.add(BatchNormalization())
-model.add(Dense(1, activation='linear'))
+        optimizer = Adam(learning_rate=0.0001)
+        model.compile(optimizer=optimizer, loss='mean_squared_error')
 
-optimizer = Adam(learning_rate=0.001)
-model.compile(optimizer=optimizer, loss='mean_squared_error')
+        return model
 
-early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
+    def train_model(self, X_train, Y_train):
+        self.model = self.build_model((X_train.shape[1], X_train.shape[2]))
+        early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
+        self.model.fit(X_train, Y_train, validation_split=0.2, epochs=200, batch_size=32, callbacks=[early_stopping], verbose=1)
 
-model.summary()
+    def predict(self, X):
+        return self.model.predict(X)
 
-# Train the model
-history = model.fit(X_data, Y_data, validation_split=0.2, epochs=200, batch_size=32, callbacks=[early_stopping], verbose=1)
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
 
-# Predictions for the dataset
-data_predict = model.predict(X_data)
+    def plot_predictions(self, actual, predicted, title):
+        plt.figure(figsize=(16, 6))
+        plt.plot(actual, label='Actual')
+        plt.plot(predicted, label='Predicted')
+        plt.ylabel('Sales_Prediction', size=13)
+        plt.xlabel('Time Step', size=13)
+        plt.tight_layout()
+        plt.legend(fontsize=13)
+        plt.title(title)
+        plt.show()
 
-# Invert the predictions to original scale
-data_predict = scaler.inverse_transform(data_predict)
-Y_data = scaler.inverse_transform([Y_data])
+    def make_future_predictions(self, data, num_predictions=60):
+        last_lookback_values = data[-self.lookback:].reshape(1, 1, self.lookback)
+        self.future_predictions = []
 
-# Plot dataset predictions
-plt.figure(figsize=(16, 6))
-plt.plot(Y_data[0], label='Actual')
-plt.plot(data_predict[:, 0], label='Predicted')
-plt.ylabel('Sales_Prediction', size=13)
-plt.xlabel('Time Step', size=13)
-plt.tight_layout()
-plt.legend(fontsize=13)
-plt.show()
+        for _ in range(num_predictions):
+            future_pred_scaled = self.model.predict(last_lookback_values)
+            self.future_predictions.append(future_pred_scaled[0, 0])
+            last_lookback_values = np.roll(last_lookback_values, -1)
+            last_lookback_values[0, 0, -1] = future_pred_scaled[0, 0]
 
-# Future predictions using the entire dataset
-last_lookback_values = data[-lookback:].reshape(1, 1, lookback)
+        self.future_predictions = self.inverse_transform(np.array(self.future_predictions).reshape(-1, 1))
 
-future_predictions = []
-num_predictions = 60  # Number of future steps to predict
+    def plot_future_predictions(self, data, num_predictions):
+        data_inverse = self.inverse_transform(data)
+        combined_data = np.concatenate((data_inverse, self.future_predictions), axis=0)
+        
+        combined_df = pd.DataFrame(combined_data, columns=['Sales_Prediction'])
+        x_original = range(len(combined_data))
+        x_future = range(len(data_inverse), len(data_inverse) + num_predictions)
 
-for _ in range(num_predictions):
-    future_pred_scaled = model.predict(last_lookback_values)
-    future_predictions.append(future_pred_scaled[0, 0])
-    last_lookback_values = np.roll(last_lookback_values, -1)
-    last_lookback_values[0, 0, -1] = future_pred_scaled[0, 0]
+        # Plot historical sales and future predictions
+        plt.figure(figsize=(16, 6))
+        plt.plot(x_original, combined_df, label='Historical Sales')
+        plt.plot(x_future, self.future_predictions, label='Future Predictions')
+        plt.ylabel('Sales Prediction', size=13)
+        plt.xlabel('Time Step', size=13)
+        plt.tight_layout()
+        plt.legend(fontsize=13)
+        plt.show()
+    
+    def future_predictions_to_dict(self):
+        return {i: float(pred[0]) for i, pred in enumerate(self.future_predictions)}
 
-future_predictions = scaler.inverse_transform(np.array(future_predictions).reshape(-1, 1))
+    def run(self, num_pred:int=30):
+        data = self.load_data()
+        X_data, Y_data = self.create_dataset(data, self.lookback)
+        X_data = np.reshape(X_data, (X_data.shape[0], 1, X_data.shape[1]))
 
-data_inverse = scaler.inverse_transform(data)
-combined_data = np.concatenate((data_inverse, future_predictions), axis=0)
+        self.train_model(X_data, Y_data)
+        
+        data_predict = self.predict(X_data)
+        data_predict = self.inverse_transform(data_predict)
+        Y_data = self.inverse_transform([Y_data])
 
-# Create a new DataFrame to plot the combined data
-combined_df = pd.DataFrame(combined_data, columns=['Sales_Prediction'])
-x_original = range(len(combined_data))
-x_future = range(len(data_inverse), len(data_inverse) + num_predictions)
+        self.plot_predictions(Y_data[0], data_predict[:, 0], "Historical Sales Predictions")
 
-# Plot historical sales and future predictions
-plt.figure(figsize=(16, 6))
-plt.plot(x_original, combined_df, label='Historical Sales')
-plt.plot(x_future, future_predictions, label='Future Predictions')
-plt.ylabel('Sales Prediction', size=13)
-plt.xlabel('Time Step', size=13)
-plt.tight_layout()
-plt.legend(fontsize=13)
-plt.show()
+        self.make_future_predictions(data, num_predictions=num_pred)
+        self.plot_future_predictions(data, num_predictions=num_pred)
+
+if __name__ == "__main__":
+    model = SalesPredictionModel(r'DataPreprocess\data\Sales Data.csv')
+    model.run(num_pred=30)
+    future_dict = model.future_predictions_to_dict
+    print(future_dict)
