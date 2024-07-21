bdce9031d32ed7dd0df932e3ad313e2b186debf0
ishaan19d <ishaandas848@gmail.com>
2024-07-21 18:45:39 +0530

    can view/delete files

--- Backend/inventory/api/urls.py
+++ Backend/inventory/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ItemsView, SalesUploadView, SalesListView
+from .views import ItemsView, SalesFileView, SalesListView
 
 urlpatterns = [
     path('items/',ItemsView.as_view(), name='items'),
-    path('upload-sales/',SalesUploadView.as_view(), name='upload-sales'),
+    path('upload-sales-file/',SalesFileView.as_view(), name='upload-sales-file'), #post
+    path('view-files/',SalesFileView.as_view(), name='view-files'), #get
+    path('delete-file/<str:file_id>/', SalesFileView.as_view(), name='sales-delete'), #delete
     path('view-sales/',SalesListView.as_view(), name='sales'),
 ]
--- Backend/inventory/api/views.py
+++ Backend/inventory/api/views.py
@@ -8,6 +8,7 @@ import pytz
 from django.conf import settings
 
 from pymongo import MongoClient
+from bson import ObjectId
 
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
@@ -36,9 +37,25 @@ class ItemsView(APIView):
             item['_id'] = str(item['_id'])
         return Response(items, status=status.HTTP_200_OK)
     
-class SalesUploadView(APIView):
+class SalesFileView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        sales_collection = db[f"{request.user.company_id}_sales"]
+
+        files = sales_collection.find()
+
+        response_data = []
+        for file in files:
+            response_data.append({
+                "filename": file['filename'],
+                "uploaded_at": file['timestamp'],
+                "file_id": str(file['file_id']),
+                "document_id": str(file['_id'])
+            })
+
+        return Response(response_data, status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
         file = request.FILES.get('file')
@@ -83,7 +100,28 @@ class SalesUploadView(APIView):
 
         except Exception as e:
             return Response({"error": f"Error storing data: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
+        
+    def delete(self, request, file_id, format=None):
+        try:
+            file_id = ObjectId(file_id)
+        except Exception as e:
+            return Response({"error": "Invalid file_id format."}, status=status.HTTP_400_BAD_REQUEST)
+
+        sales_collection = db[f"{request.user.company_id}_sales"]
+
+        file_doc = sales_collection.find_one({"file_id": file_id})
+        if not file_doc:
+            return Response({"error": "File not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            settings.FS.delete(file_id)
+            sales_collection.delete_one({"file_id": file_id})
+
+            return Response({"message": "File deleted successfully."}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            
 class SalesListView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
