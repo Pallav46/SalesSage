ef6bd4bc24ce3f43151263adb9372f811e9f690e
ishaan19d <ishaandas848@gmail.com>
2024-07-29 02:29:47 +0530

    Version 1

--- Backend/accounts/api/views.py
+++ Backend/accounts/api/views.py
@@ -45,7 +45,7 @@ class SendOTP(APIView):
                 'expiry_time': expiry_time
             })
         try:
-            send_otp_email(email, otp, "welcome to SalesSage", "hii")
+            send_otp_email(email, otp, "Welcome to SalesSage", "To get you started, ")
         except:
             otp_collection.delete_one({'email': email})
             return Response({"error": "Failed to send OTP"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
--- Backend/inventory/tasks.py
+++ Backend/inventory/tasks.py
@@ -11,13 +11,11 @@ import pandas as pd
 from inventory.api.ML.dataPreprocessing import DataPreprocessing
 from inventory.api.ML.getPredictions import GetPredictions
 logger = logging.getLogger(__name__)
-import time
 
 @shared_task
 def async_start_training(company_id):
     channel_layer = get_channel_layer()
     try:
-        time.sleep(10)
         async_to_sync(channel_layer.group_send)(
             f'training_status_{company_id}',
             {
@@ -26,7 +24,6 @@ def async_start_training(company_id):
             }
         )
         logger.info("Async training started for company_id: %s", company_id)
-<<<<<<< HEAD
         # Your existing training code here
         db = settings.DB
         sales_collection = db[f"{company_id}_sales"]
@@ -49,7 +46,7 @@ def async_start_training(company_id):
         dp = DataPreprocessing()
         df = dp.process_data(df)
         logger.info("Prediction will start now for company_id: %s", company_id)
-        gp = GetPredictions(df,tier=3)
+        gp = GetPredictions(df)
         all_predictions = gp.predictions()
 
         predictions_collection = db[f"{company_id}_predictions"]
@@ -64,48 +61,7 @@ def async_start_training(company_id):
         cache_key = f"forecast_{company_id}"
         forecast_json = json.loads(json_util.dumps(all_predictions))
         cache.set(cache_key, json.dumps(forecast_json), timeout=3600)
-=======
->>>>>>> 8887402404ac8f448da071445824d0a46db03ee4
-        
-        # Your existing training code here
-        # db = settings.DB
-        # sales_collection = db[f"{company_id}_sales"]
-        # fs = settings.FS
 
-        # all_documents = list(sales_collection.find())
-        # all_data = []
-
-        # for document in all_documents:
-        #     file_id = document.get('file_id')
-        #     if file_id:
-        #         grid_out = fs.get(file_id)
-        #         json_data = grid_out.read().decode('utf-8')
-        #         data = json.loads(json_data)
-        #         all_data.extend(data)
-
-        # df = pd.DataFrame(all_data)
-        # df = df.drop_duplicates()
-
-        # dp = DataPreprocessing()
-        # df = dp.process_data(df)
-        # logger.info("Prediction will start now for company_id: %s", company_id)
-        # gp = GetPredictions(df)
-        # all_predictions = gp.predictions()
-
-        # predictions_collection = db[f"{company_id}_predictions"]
-        # predictions_collection.delete_many({})
-
-        # prediction_document = {
-        #     "date": datetime.now(),
-        #     "predictions": all_predictions
-        # }
-        # predictions_collection.insert_one(prediction_document)
-
-        # cache_key = f"forecast_{company_id}"
-        # forecast_json = json.loads(json_util.dumps(all_predictions))
-        # cache.set(cache_key, json.dumps(forecast_json), timeout=3600)
-        
-        time.sleep(10)
         async_to_sync(channel_layer.group_send)(
             f'training_status_{company_id}',
             {
